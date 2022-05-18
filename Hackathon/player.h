819a57c41089da5c1cb7f8f15d7589dc3db95a6e
bogdanmaciuca <103450581+bogdanmaciuca@users.noreply.github.com>
2022-05-18 17:53:38 +0300

    Add files via upload

--- Hackathon/player.h
+++ Hackathon/player.h
@@ -15,11 +15,10 @@ private:
 	bool m_holding_jump = 0;
 	float m_hold_jump_timer = 0.0f;
 	float m_max_hold_jump_time = 300.0f;
-	float m_gravity = .01f;
+	float m_gravity = 0.01f;
 	bool m_ground = 0;
 	bool m_aux_jump_cmd = 0;
 
-	float m_timejump = 21, grav = 0.25f, jumpforce = -7.0f, adjustjump = 35.0f;
 	float m_max_horiz_vel = 5.0f;
 	float m_accel = 0.08f;
 	float m_decel = 0.1f;
@@ -32,8 +31,9 @@ public:
 		m_velocity_x = 0, m_velocity_y = 0;
 	}
 	void SetNormalStats() {
-		m_timejump = 21, grav = 0.25f, jumpforce = -7.0f, adjustjump = 35.0f;
+		m_jump_velocity = -1.0f;
 		m_max_horiz_vel = 5.0f;
+		m_max_hold_jump_time = 300.0f;
 		m_accel = 0.08f;
 		m_decel = 0.1f;
 	}
@@ -44,28 +44,30 @@ public:
 	}
 	void SetRandomJumpStats() {
 		SetNormalStats();
-		jumpforce = -10.0f;
+		m_jump_velocity = -1.5f;
 	}
 	void SetBunnyhopStats() {
 		SetNormalStats();
-		jumpforce = -10.0f;
+		m_jump_velocity= -1.5f;
 		m_accel = 0.16f;
 		m_decel = 0.15f;
 	}
 	void SetNoLeftStats() {
 		SetNormalStats();
-		jumpforce = -10.0f;
+		m_jump_velocity = -1.5f;
 		m_decel = 0.05f;
 	}
 	void SetBigJumpsStats() {
 		SetNormalStats();
-		grav = 0.125f;
-		jumpforce = -10.0f;
+		m_gravity = 0.005f;
+		m_jump_velocity = -1.5f;
 	}
 	void SetGlidingStats() {
 		SetNormalStats();
-		//m_timejump = 21.0f;
-		//adjustjump = 100.0f;
+	}
+	void SetSpeedStats() {
+		SetNormalStats();
+		m_max_horiz_vel = 10.0f;
 	}
 	Player(Graphics* graphics, char* _mode) {
 		gfx = graphics;
@@ -82,12 +84,13 @@ public:
 	void UpdateInput(Input* input, bool no_left = 0) {
 		if (!no_left && input->GetKeyDown('A')) m_velocity_x -= m_accel;
 		else if (input->GetKeyDown('D')) m_velocity_x += m_accel;
-		if (!(!no_left && input->GetKeyDown('A')) && !input->GetKeyDown('D'))
-		{
+		if (!(!no_left && input->GetKeyDown('A')) && !input->GetKeyDown('D')) {
 			if (m_velocity_x > 0)
 				m_velocity_x -= m_decel;
 			else
 				m_velocity_x += m_decel;
+			if (abs(m_velocity_x) <= 2 * m_decel)
+				m_velocity_x = 0;
 		}
 		if (m_velocity_x <= -m_max_horiz_vel)
 			m_velocity_x = -m_max_horiz_vel;
@@ -96,8 +99,7 @@ public:
 	}
 	void UpdateGravity(const std::vector<Platform>& platforms, Input* input, float delta_time, bool random_jump) {
 		bool colision = 0;
-		//m_ground= 0;
-
+		
 		float ground_height = 2000.0f;
 		int i;
 		for (i = 0; i < PLATFORM_NUM_ON_SCR && !colision; i++) {
@@ -135,7 +137,7 @@ public:
 			if (!m_holding_jump) {
 				m_velocity_y += m_gravity * delta_time;
 			}
-			if (m_y >= ground_height - PLAYER_HEIGHT) {
+			if (m_y >= ground_height - PLAYER_HEIGHT && m_y <= ground_height + PLAYER_HEIGHT / 2) {
 				m_y = ground_height - PLAYER_HEIGHT;
 				m_ground = 1;
 			}
